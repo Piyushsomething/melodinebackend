40df1ffb5ffbf9f0af0004648a32489bd3f9b75d
piyush-pilnu <piyush@32mobiles.com>
2024-03-27 19:45:54 +0530

    render deployed

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from categories import categories_router
 from songs import songs_router
 from about import about_router
 from artist import artists_router
 from popularsongs import popular_songs_router
 
-
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this based on your requirements
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 app.include_router(categories_router, prefix="/api")
 app.include_router(songs_router, prefix="/api")
 app.include_router(about_router, prefix="/api")
